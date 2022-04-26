1ad068ab38b1d45bce9fa10795cdd9c9a11f2d31
MateusP21 <mateuspasso@outlook.com>
2022-04-26 18:15:35 -0300

    mexendo nas paginas

--- src/App.js
+++ src/App.js
@@ -1,30 +1,36 @@
 import React from 'react';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { BrowserRouter, Route } from 'react-router-dom';
+import Foods from './pages/Foods';
 import './styles/App.css';
 import rockGlass from './images/rockGlass.svg';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import Login from './pages/Login';
+import Drinks from './pages/Drinks';
+import Explore from './pages/Explore';
+import Profile from './pages/Profile';
+import DoneRecipes from './pages/DoneRecipes';
+import FavoriteRecipes from './pages/FavoriteRecipes';
 
 function App() {
   return (
     <BrowserRouter>
-      <Switch>
-        <Route exact path="/" component={ Login } />
-        <Route exact path="/foods" component={ Foods } />
-        <Route exact path="/drinks" component={ Drinks } />
-        <Route exact path="/foods/:id" component={ Food } />
-        <Route exact path="/drinks/:id" component={ Drink } />
-        <Route exact path="/foods/:id/in-progress " component={ Foods } />
-        <Route exact path="/drinks/:id/in-progress " component={ Foods } />
-        <Route exact path="/explore" component={ Explore } />
-        <Route exact path="/explore/foods" component={ Foods } />
-        <Route exact path="/explore/drinks" component={ Foods } />
-        <Route exact path="/explore/foods/ingredients" component={ Foods } />
-        <Route exact path="/explore/drinks/ingredients" component={ Foods } />
-        <Route exact path="/explore/foods/nationalities" component={ Foods } />
-        <Route exact path="/profile" component={ Profile } />
-        <Route exact path="/done-recipes" component={ Login } />
-        <Route exact path="/favorite-recipes " component={ Login } />
-      </Switch>
+      <Route exact path="/" component={ Login } />
+      <Route path="/foods" component={ Foods } />
+      <Route path="/drinks" component={ Drinks } />
+      <Route path="/foods/:id" component={ Foods } />
+      <Route path="/drinks/:id" component={ Drinks } />
+      <Route path="/foods/:id/in-progress " component={ Foods } />
+      <Route path="/drinks/:id/in-progress " component={ Drinks } />
+      <Route path="/explore" component={ Explore } />
+      <Route path="/explore/foods" component={ Explore } />
+      <Route path="/explore/drinks" component={ Explore } />
+      <Route path="/explore/foods/ingredients" component={ Explore } />
+      <Route path="/explore/drinks/ingredients" component={ Explore } />
+      <Route path="/explore/foods/nationalities" component={ Explore } />
+      <Route path="/profile" component={ Profile } />
+      <Route path="/done-recipes" component={ DoneRecipes } />
+      <Route path="/favorite-recipes" component={ FavoriteRecipes } />
+
       <div className="meals">
         <span className="logo">TRYBE</span>
         <object
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,17 @@
 import React from 'react';
+import profileIcon from '../images/profileIcon.svg';
+import searchIcon from '../images/searchIcon.svg';
 
 const Header = () => (
-  <h1>Header</h1>
+  <header className="header">
+    <button type="button">
+      <img data-testid="profile-top-btn" src={ profileIcon } alt="profile button" />
+    </button>
+    <h1 data-testid="page-title">Foods</h1>
+    <button type="button">
+      <img data-testid="search-top-btn" src={ searchIcon } alt="search button" />
+    </button>
+  </header>
 );
 
 export default Header;
