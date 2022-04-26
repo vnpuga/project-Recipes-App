0527ca763c8998c860e1e50db07260790d241d5e
Letícia Oliveira Andrade <leticiaoliveiraandrade.9@gmail.com>
2022-04-26 19:07:49 -0300

    Requisito 2 - Tela de Login

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,5 +1,32 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-const Login = () => <h1>Login</h1>;
+const Login = () => {
+  return (
+    <form>
+      <label htmlFor="email-input">
+        email
+        <input
+          type="text"
+          id="email-input"
+          data-testid="email-input"
+        />
+      </label>
+      <label htmlFor="password-input">
+        password
+        <input
+          type="text"
+          id="password-input"
+          data-testid="password-input"
+        />
+      </label>
+      <button
+        type="button"
+        data-testid="login-submit-btn"
+      >
+        Enter
+      </button>
+    </form>
+  );
+};
 
 export default Login;
