1b24518e5a86bda2146dcaee318a334f0ba157b5
MateusP21 <mateuspasso@outlook.com>
2022-04-26 21:26:37 -0300

    finalizando header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,17 +1,39 @@
-import React from 'react';
+import React, { useState } from 'react';
+import PropTypes from 'prop-types';
+import { useHistory } from 'react-router-dom';
 import profileIcon from '../images/profileIcon.svg';
 import searchIcon from '../images/searchIcon.svg';
 
-const Header = () => (
-  <header className="header">
-    <button type="button">
-      <img data-testid="profile-top-btn" src={ profileIcon } alt="profile button" />
-    </button>
-    <h1 data-testid="page-title">Foods</h1>
-    <button type="button">
-      <img data-testid="search-top-btn" src={ searchIcon } alt="search button" />
-    </button>
-  </header>
-);
+const Header = ({ title, searchButton = true }) => {
+  const { push } = useHistory();
+  const [showSearchBar, setShowSearchBar] = useState(false);
+
+  return (
+    <header className="header">
+      <button onClick={ () => push('/profile') } type="button">
+        <img data-testid="profile-top-btn" src={ profileIcon } alt="profile button" />
+      </button>
+      <h1 data-testid="page-title">{title}</h1>
+      {
+        searchButton && (
+          <button onClick={ () => setShowSearchBar(!showSearchBar) } type="button">
+            <img data-testid="search-top-btn" src={ searchIcon } alt="search button" />
+          </button>)
+      }
+
+      {
+        showSearchBar && (
+          <input type="text" data-testid="search-input" />
+        )
+      }
+    </header>);
+};
 
 export default Header;
+Header.propTypes = {
+  title: PropTypes.string.isRequired,
+  searchButton: PropTypes.bool.isRequired,
+  history: PropTypes.shape({
+    push: PropTypes.func,
+  }).isRequired,
+};
